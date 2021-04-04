ffaecc05d9049ed4dab6cce7ae28d8ca28255876
joselvelez <joselvelez@protonmail.com>
2021-04-04 13:21:07 -0500

    completed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CartItem from './CartItem';
 import './Cart.css';
 
 function Cart({initialData}) {
+
+    // Set initial state
+    const initialState = JSON.parse(window.localStorage.getItem('items'));
+
+    // Set state for items in cart
+    const [items, setItems] = useState(initialState || initialData);
+
+    // Add local storage for data persistence using
+    // React Hook useEffect. Leverage the  dependency
+    // array that useEffect has to update localstorage
+    // only when state of items changes.
+    useEffect( () => {
+        window.localStorage.setItem('items', JSON.stringify(items))
+    }, [items]);
+
+    // Helper function to calculate grand total
+    const grandTotal = items.reduce((total, item) => {
+        return total + (item.price * item.qty);
+    }, 0).toFixed(2) 
+
+    // Function to update quantity that gets passed down to
+    // the child component, CartItem, as props.
+    const updateQty = (itemId, newQty) => {
+        // Create a copy of the initial state to update it.
+        // One should never mutate the original state. React
+        // needs it to compare changes.
+        let newItems = items.map(item => {
+            if (itemId === item.id) {
+                return {...item, qty: newQty};
+            }
+            return item;
+        });
+
+        setItems(newItems);
+        }
+
     return(
         <div className="Cart">
             <h4>Cart Items</h4>
-            {initialData.map(item => {
-                return(
-                    <div key={item.id}>
-                        <CartItem name={item.name} price={item.price} qty={item.qty} />
-                    </div>
-                )
-            })}
+            <div className="CartItems">
+                {items.map(item => {
+                    return <CartItem key={item.id} {...item} updateQty={updateQty} />
+                })}
+            </div>
+            <div className="GrandTotal">
+                Grand Total: ${grandTotal}
+            </div>
         </div>
     );
 }
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,16 +2,29 @@ import React from 'react';
 import './CartItem.css';
 
 function CartItem(props) {
+
+    // Instead of passing the updateQty function from the parent
+    // component into the CartItem component, make a function
+    // for adding/subtracting from qty here and pass into the
+    // respective button
+    const addOne = () => props.updateQty(props.id, props.qty + 1);
+    const subOne = () => props.updateQty(props.id, props.qty - 1);
+
     return(
         <div className="CartItem">
             <div className="CartItemName">
                 {props.name}
             </div>
             <div className="CartItemPrice">
-                {props.price}
+                ${props.price.toFixed(2)}
             </div>
             <div className="CartItemQty">
-                {props.qty}
+                <button onClick={addOne}>+</button>
+                {` ${props.qty} `}
+                <button onClick={subOne} disabled={props.qty < 1}>-</button>
+            </div>
+            <div className="CartItemSubtotal">
+                Total: ${(props.qty * props.price).toFixed(2)}
             </div>
         </div>
     );
